77df5416230d02ade516047535805070eb67ba95
Yuchen_Yao <s3548974@student.rmit.edu.au>
2018-10-03 16:40:02 +1000

    update version

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
- hello
+<?php 
+  session_start();
+  if(isset($_SESSION['username'])){
+	  header("location: index.html");
+  }
+  if(!isset($_SESSION['username'])){
+	  $_SESSION['msg']="You must be log in first";
+	  header('location:login.php');
+  }
+  
+  ?>
