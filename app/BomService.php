65f606a4e694e0d464e6f162d7c88f2c3280cc56
nolDsouza <s3600251@student.rmit.edu.au>
2018-10-12 08:12:36 +1100

    hotfix on link - missing slash

--- app/BomService.php
+++ app/BomService.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Storage;
 class BomService {
 
     public function getXML($target) {
-        $forecasts = 'ftp://ftp.bom.gov.au/anon/gen/fwo';
+        $forecasts = 'ftp://ftp.bom.gov.au/anon/gen/fwo/';
         $url = $forecasts . $target;
         $data = file_get_contents($url);
         return $data;
